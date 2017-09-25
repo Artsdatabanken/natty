55b5a35dffae29246808cc39cd10f2615b4f4462
jarle pedersen <jarle.pedersen@norconsult.com>
2017-09-25 15:39:28 +0200

    basic interface for selecting values

--- RødlisteKlassifiserer/Form1.Designer.cs
+++ RødlisteKlassifiserer/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.UpdateButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // UpdateButton
@@ -41,21 +47,84 @@
             this.UpdateButton.UseVisualStyleBackColor = true;
             this.UpdateButton.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(30, 71);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(97, 454);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NaturområdeType";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Beskrivelsesvariabel";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(197, 71);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(97, 454);
+            this.checkedListBox2.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 570);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.UpdateButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RødlisteKlassifiserer/Form1.cs
+++ RødlisteKlassifiserer/Form1.cs
@@ -17,5 +17,29 @@ namespace Forms_dev3
             CodeDeserializer.UpdateNaturtypeFromWeb();
             CodeDeserializer.UpdateBeskrivelsesvariablerFromWeb();
         }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            using (var Context = new RødlistedeNaturtyperKlassifiseringContainer())
+            {
+                foreach (var hit in Context.NaturområdeTypeKodeSet.Where(d => (d.verdi + "-" + d.KartleggingsKode.verdi).StartsWith(textBox1.Text.ToUpper()) && d.KartleggingsKode.nivå == "A"))
+                {
+                    checkedListBox1.Items.Add(hit.verdi + "-" + hit.KartleggingsKode.verdi);
+                }
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            checkedListBox2.Items.Clear();
+            using (var Context = new RødlistedeNaturtyperKlassifiseringContainer())
+            {
+                foreach (var hit in Context.BeskrivelsesvariabelSet.Where(d => d.verdi.StartsWith(textBox2.Text)))
+                {
+                    checkedListBox2.Items.Add(hit.verdi);
+                }
+            }
+        }
     }
 }
