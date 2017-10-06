a95347842cc03df8f0572938be182ed5ef29a2e0
jarle pedersen <jarle.pedersen@norconsult.com>
2017-10-06 14:16:43 +0200

    minor refactoring and tidying

--- NiNCoreKlassifiserer/Form1.cs
+++ NiNCoreKlassifiserer/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.Entity;
-using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using RødlisteKlassifiserer;
 
@@ -15,6 +10,9 @@ namespace NiNCoreKlassifiserer
 {
     public partial class Form1 : Form
     {
+        private static readonly Dictionary<RødlisteKlassifisering, List<Naturområde>> RødlistedeNaturområderDict =
+            new Dictionary<RødlisteKlassifisering, List<Naturområde>>();
+
         public Form1()
         {
             InitializeComponent();
@@ -22,14 +20,14 @@ namespace NiNCoreKlassifiserer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            var rødlistedeNaturområderDict = new Dictionary<RødlisteKlassifisering, List<Naturområde>>();
+            ClassifyRødlistedeOmråder();
+        }
 
+        private static void ClassifyRødlistedeOmråder()
+        {
             foreach (var rødlisteKlassifisering in RødlisteKlassifiserer.DataConnection.Context
                 .RødlisteKlassifiseringSet)
             {
-                var rødlistedeNaturområder = new List<Naturområde>();
-
                 string naturområdeTypeKodeVerdi;
 
                 List<NaturområdeType> naturområdeTyper = null;
@@ -53,37 +51,46 @@ namespace NiNCoreKlassifiserer
 
                 if (naturområdeTyper == null) continue;
 
-                foreach (var naturområdeType in naturområdeTyper)
-                {
-                    if (rødlisteKlassifisering.Beskrivelsesvariabel.Count <= 0)
-                    {
-                        rødlistedeNaturområder.Add(naturområdeType.Naturområde);
-                        continue;
-                    }
-                    if (rødlisteKlassifisering.Beskrivelsesvariabel.Count == 1 &&
-                        naturområdeType.Beskrivelsesvariabel.Any(d =>
-                            d.kode == rødlisteKlassifisering.Beskrivelsesvariabel.First().verdi))
-                    {
-                        rødlistedeNaturområder.Add(naturområdeType.Naturområde);
-                        continue;
-                    }
-                    var allBeskrivelsesvariablerPresent = true;
-                    foreach (var beskrivelsesvariabel in rødlisteKlassifisering.Beskrivelsesvariabel)
-                    {
-                        if (naturområdeType.Beskrivelsesvariabel.Any(d => d.kode == beskrivelsesvariabel.verdi))
-                            continue;
-                        allBeskrivelsesvariablerPresent = false;
-                        break;
-                    }
-                    if (allBeskrivelsesvariablerPresent) rødlistedeNaturområder.Add(naturområdeType.Naturområde);
-                }
-
-                foreach (var naturområde in rødlistedeNaturområder)
-                {
-                    if(!rødlistedeNaturområderDict.ContainsKey(rødlisteKlassifisering)) rødlistedeNaturområderDict[rødlisteKlassifisering] = new List<Naturområde>();
-                    rødlistedeNaturområderDict[rødlisteKlassifisering].Add(naturområde);
-                }
+                CheckNaturområdeTypeForBeskrivelsesvariabel(rødlisteKlassifisering, naturområdeTyper);
             }
+        }
+
+        private static void CheckNaturområdeTypeForBeskrivelsesvariabel(RødlisteKlassifisering rødlisteKlassifisering,
+            List<NaturområdeType> naturområdeTyper)
+        {
+            foreach (var naturområdeType in naturområdeTyper)
+            {
+                if (rødlisteKlassifisering.Beskrivelsesvariabel.Count <= 0)
+                {
+                    AddNaturområdeToDictionary(rødlisteKlassifisering, naturområdeType.Naturområde);
+                    continue;
+                }
+                if (rødlisteKlassifisering.Beskrivelsesvariabel.Count == 1 &&
+                    naturområdeType.Beskrivelsesvariabel.Any(d =>
+                        d.kode == rødlisteKlassifisering.Beskrivelsesvariabel.First().verdi))
+                {
+                    AddNaturområdeToDictionary(rødlisteKlassifisering, naturområdeType.Naturområde);
+                    continue;
+                }
+                var allBeskrivelsesvariablerPresent = true;
+                foreach (var beskrivelsesvariabel in rødlisteKlassifisering.Beskrivelsesvariabel)
+                {
+                    if (naturområdeType.Beskrivelsesvariabel.Any(d => d.kode == beskrivelsesvariabel.verdi))
+                        continue;
+                    allBeskrivelsesvariablerPresent = false;
+                    break;
+                }
+                if (allBeskrivelsesvariablerPresent)
+                    AddNaturområdeToDictionary(rødlisteKlassifisering, naturområdeType.Naturområde);
+            }
+        }
+
+        private static void AddNaturområdeToDictionary(RødlisteKlassifisering rødlisteKlassifisering,
+            Naturområde naturområde)
+        {
+            if (!RødlistedeNaturområderDict.ContainsKey(rødlisteKlassifisering))
+                RødlistedeNaturområderDict[rødlisteKlassifisering] = new List<Naturområde>();
+            RødlistedeNaturområderDict[rødlisteKlassifisering].Add(naturområde);
         }
 
         private static IEnumerable<NaturområdeType> GetNaturområdeTyper(string naturområdeTypeKodeVerdi)
@@ -103,7 +110,7 @@ namespace NiNCoreKlassifiserer
         {
             if (!dbSet.Any()) return dbSet.Add(entity);
 
-            var existingRows = dbSet.Where(predicate);
+            var existingRows = dbSet.Where(predicate ?? throw new ArgumentNullException(nameof(predicate)));
             if (!existingRows.Any()) return dbSet.Add(entity);
 
             Context.Entry(existingRows.First()).State = EntityState.Unchanged;
@@ -111,4 +118,3 @@ namespace NiNCoreKlassifiserer
         }
     }
 }
-
